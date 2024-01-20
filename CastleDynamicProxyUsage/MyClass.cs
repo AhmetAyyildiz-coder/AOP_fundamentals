5d53ecc91cdcc4f249f8d7f690d050e74b4fbb7e
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2024-01-21 02:22:10 +0300

    Temel olarak araya girmesi beklenilen kod bloğu oluşturuldu.

---
+++ CastleDynamicProxyUsage/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using Castle.DynamicProxy;
+
+namespace CastleDynamicProxyUsage;
+
+
+public class MyClass
+{
+    // Böyle tanım olursa interception - dynamic proxy kullanarak araya giremez. 
+    //public void MyMethod()
+    //{
+
+    //}
+
+
+    // araya girme - aspect - için mutlaka virtual olmalıdır. 
+
+    public virtual void MyMethod()
+    {
+        Console.WriteLine("My method body");
+    }
+}
+
+
+
+// eğer bu sınıfın bir interceptor olmasını istiyorsak mutlaka bu interface'den kalıtım alınmalı
+public class MyInterceptorAspect : IInterceptor
+{
+    // invocation.Method ile method adini alabiliyoruz. Dynamic şekilde. Mükemmel :)
+    public void Intercept(IInvocation invocation)
+    {
+        Console.WriteLine($"Before method {invocation.Method}");
+        // araya girdiğimiz metodu devam ettirmek istediğimiz için bu şekilde devam ederç
+        invocation.Proceed();
+        Console.WriteLine($"After method {invocation.Method}");
+
+    }
+}
---
+++ CastleDynamicProxyUsage/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+ *En temel düzeyde Castle dynamic proxy tanımı
+ *
+ * Runtime'da proxy tanımlaya yarayan bir teknolojidir.
+ * Hafif proxy'ler oluşturmaya yarar. Nesnenin kodunu değiştirmeden üyelerine erişmemizi ve kontrolleri yapabilmemizi sağlar.
+ * Bu sayede aop'yi kullanabilmemizi sağlar.
+ *
+ * Intercaptions'lar virtual'ların içerisine girebilir sadece.
+ * Bu konu önemli. VİRTUAL mutlaka kullanılmalı.
+ *
+ * Bu projede en yalın hali ile bir aspect yapacağız.
+ *
+ */
