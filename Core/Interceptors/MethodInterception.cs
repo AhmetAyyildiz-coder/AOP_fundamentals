0c73ca370b6397cba194418c873b381cbcfab140
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2024-01-21 04:46:19 +0300

    custom method interceptor yazımı -

---
+++ Core/Interceptors/MethodInterception.cs
@@ -0,0 +1,45 @@
+﻿using Castle.DynamicProxy;
+
+namespace Core.Interceptors;
+
+/// <summary>
+/// kendi custom method interception'umuz. Artık bir method'un tetiklenmesindeki lifetime'ı boyunca yönetimini sağlayabiliriz.
+/// </summary>
+public class MethodInterception : MethodInterceptionBaseAttribute
+{
+    
+    public override void Intercept(IInvocation invocation)
+    {
+        bool succesFlag = true;
+        OnBefore(invocation);
+
+        try
+        {
+            invocation.Proceed();
+        }
+        catch (Exception e)
+        {
+            succesFlag = false;
+            OnException(invocation, e);
+            throw;
+        }
+        finally
+        {
+            if (succesFlag)
+            {
+                OnSuccess(invocation);
+            }
+        }
+        OnAfter(invocation);
+        base.Intercept(invocation);
+    }
+
+    public virtual void OnBefore(IInvocation invocation) { }
+
+    public virtual void OnAfter(IInvocation invocation) { }
+    public virtual void OnException(IInvocation invocation,Exception e) { }
+
+    public virtual void OnSuccess(IInvocation invocation) { }
+
+
+}
---
+++ Core/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+
+namespace Core.Interceptors;
+
+
+/*
+ * Bundan sonra bir aspect yazmak istediğimde bir base olarak bu sınıfı kullanabiliriz.
+ */
+
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Assembly)]
+public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+{
+    // interceptor özelliğinin çalışabilmesi için mutlaka virtual özelliği verilmeli !!!
+
+
+    /// <summary>
+    /// Çalışma önceliği ,
+    ///  Eğer birden fazla attribute tanımlanırsa öncelik vermek için. 
+    /// </summary>
+    public int Priority { get; set; }
+
+    public virtual void Intercept(IInvocation invocation)
+    {
+    }
+}
