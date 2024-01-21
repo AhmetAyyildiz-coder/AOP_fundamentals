43436bff5d43c9c8d8c445be75b6a04b5df09d9d
Ahmet Ayyildiz <84086596+AhmetAyyildiz-coder@users.noreply.github.com>
2024-01-21 04:48:21 +0300

    yeni yazdığımız method interceptor ile var olan kodları düzenleme

--- CastleDynamicProxyUsage/MyClass.cs
+++ CastleDynamicProxyUsage/MyClass.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using Castle.DynamicProxy;
+using Core.Interceptors;
 
 namespace CastleDynamicProxyUsage;
 
@@ -24,15 +25,32 @@ public class MyClass
 
 
 // eğer bu sınıfın bir interceptor olmasını istiyorsak mutlaka bu interface'den kalıtım alınmalı
-public class MyInterceptorAspect : IInterceptor
+//public class MyInterceptorAspect : IInterceptor
+//{
+//    // invocation.Method ile method adini alabiliyoruz. Dynamic şekilde. Mükemmel :)
+//    public void Intercept(IInvocation invocation)
+//    {
+//        Console.WriteLine($"Before method {invocation.Method}");
+//        // araya girdiğimiz metodu devam ettirmek istediğimiz için bu şekilde devam ederç
+//        invocation.Proceed();
+//        Console.WriteLine($"After method {invocation.Method}");
+
+//    }
+//}
+
+
+// V2 olarak guncellersek 
+public class MyInterceptorAspect : MethodInterception
 {
-    // invocation.Method ile method adini alabiliyoruz. Dynamic şekilde. Mükemmel :)
-    public void Intercept(IInvocation invocation)
+    // burada   invocation.Proceed(); kullanmaya gerek kalmadı. Çünkü MethodInterception zaten default olarak yapıyor bu işlemi.
+
+    public override void OnBefore(IInvocation invocation)
     {
         Console.WriteLine($"Before method {invocation.Method}");
-        // araya girdiğimiz metodu devam ettirmek istediğimiz için bu şekilde devam ederç
-        invocation.Proceed();
-        Console.WriteLine($"After method {invocation.Method}");
+    }
 
+    public override void OnAfter(IInvocation invocation)
+    {
+        Console.WriteLine($"After method {invocation.Method}");
     }
 }
